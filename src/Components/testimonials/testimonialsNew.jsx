b1b69d1859c79cce0eb7800f9788527d6f267cf9
Usama1100 <usamaiftikhar59@gmail.com>
2024-04-09 04:17:39 +0500

    testimonials added

--- src/Components/testimonials/testimonialsNew.jsx
+++ src/Components/testimonials/testimonialsNew.jsx
@@ -4,14 +4,19 @@ import RatingImg from '../../images/rating.webp';
 import Slider from 'react-slick';
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+import daniel_photo from '../../images/daniel-photo.webp'
+import mr_sean_photo from '../../images/mr-sean-photo.webp'
+import ithrow_photo from '../../images/ithrow-photo.webp'
+import natasha_photo from '../../images/natasha-photo.webp'
+import jonas_photo from '../../images/jonas-photo.webp'
 
 export default function TestimonialsNew() {
   const testimonials = [
-    { id: 1, name: 'Leslie Alexander', role: 'Freelance React Developer', content: 'You made it so simple. My new site is so much faster and easier to work with than my old site. I just choose the page, make the change.' },
-    { id: 2, name: 'Leslie Alexander', role: 'Freelance React Developer', content: 'You made it so simple. My new site is so much faster and easier to work with than my old site. I just choose the page, make the change.' },
-    { id: 3, name: 'Leslie Alexander', role: 'Freelance React Developer', content: 'You made it so simple. My new site is so much faster and easier to work with than my old site. I just choose the page, make the change.' },
-    { id: 4, name: 'Leslie Alexander', role: 'Freelance React Developer', content: 'You made it so simple. My new site is so much faster and easier to work with than my old site. I just choose the page, make the change.' },
-    { id: 5, name: 'Leslie Alexander', role: 'Freelance React Developer', content: 'You made it so simple. My new site is so much faster and easier to work with than my old site. I just choose the page, make the change.' },
+    { id: 1, name: 'Esther', role: 'South Africa', content: 'It was my pleasure to work with such a professional web developer who knows how to work with a non technical person. Loved the end results.', image: mr_sean_photo },
+    { id: 2, name: 'Daniel Steven', role: 'United States', content: "Great job! I highly recommend for any web design/development job that you have. I've worked with him numerous times. He never disappoints.", image: daniel_photo },
+    { id: 3, name: 'Jacknaan', role: 'United States', content: 'He is a very good communicator and makes sure that he is giving you the best website and product to make your money worth it. Highly recommend.', image: jonas_photo },
+    { id: 4, name: 'Natacha', role: 'United States', content: 'He is amazing communication was excellent i will continue to business with him. He is very talented and works hard for his clients to meet their satisfaction.', image: natasha_photo },
+    { id: 5, name: 'Itrowbridge', role: 'United States', content: 'Great communication and flexibility! He was always quick to respond, super polite, and able to provide advice and be flexible to my changing ideas.', image: ithrow_photo },
   ];
 
   const settings = {
@@ -65,7 +70,7 @@ export default function TestimonialsNew() {
                     </blockquote>
                   </div>
                   <div className="testimonial-author">
-                    <img className="author-avatar" src="https://cdn.rareblocks.xyz/collection/clarity/images/testimonial/4/avatar-male-1.png" alt={testimonial.name} />
+                    <img className="author-avatar" src={testimonial.image} alt={testimonial.name} />
                     <div className="author-info">
                       <p className="author-name">{testimonial.name}</p>
                       <p className="author-role">{testimonial.role}</p>
